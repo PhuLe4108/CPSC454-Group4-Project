6ae1fba1b5bd746573ba48f1d2daa51a1607f18f
Daniel Le <PhuLe4108@csu.fullerton.edu>
2024-04-18 16:10:50 -0700

    Create a connection to mysql database

--- endUserValidateOTP.py
+++ endUserValidateOTP.py
@@ -1,5 +1,6 @@
 import socket
 import time
+import mysql.connector
 
 def verify_otp_with_server(otp):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
@@ -9,11 +10,34 @@ def verify_otp_with_server(otp):
         client.sendall(otp.encode())  # Send the actual OTP
         response = client.recv(1024).decode()  # Receive the response
         return response
+    
+def database_connection():
+    #TODO: Make sure to change the host, user, password, and database parameters 
+    database = mysql.connector.connect(
+        host="localhost",
+        user="yourusername",
+        password="yourpassword",
+        database="mydatabase"
+    )
+    ########
+
+    mycursor = database.cursor()
+
+    #TODO: Run your mysql query here to get the user's resource
+    mycursor.execute("SELECT * FROM user") #example
+    queryResult = mycursor.fetchall()
+    return queryResult
 
 def main():
     otp_input = input("Please enter the OTP you received: ").strip()
     response = verify_otp_with_server(otp_input)
     print(f"Server response: {response}")
+
+    if response == "Token verified successfully!":
+        db_resource = database_connection()
+
+    print(f"Here is your resource:\n{db_resource}")
+
 
 if __name__ == "__main__":
     main()

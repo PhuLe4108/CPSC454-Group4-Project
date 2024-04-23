d40566c3236acbc38cf2f4b62fc125467d1b65ae
Mike Thai <mikethai126@gmail.com>
2024-04-22 17:41:22 -0700

    add verify credential function

--- endUserValidateOTP.py
+++ endUserValidateOTP.py
@@ -1,43 +1,41 @@
 import socket
 import time
-import mysql.connector
+
+i = 0
+
+def verify_cred(name, pssw):
+    file = open("user.txt", "r")
+    cred = file.read().splitlines()
+    if cred[0] == name and cred[1] == pssw:
+        return True
+    
+    print("Wrong username or password!")
+    return False
+
 
 def verify_otp_with_server(otp):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
-        client.connect(('localhost', 9999))
+        client.connect(('192.168.142.4', 9999))
         client.sendall(b"verify_otp")  # Inform server of the operation type
         time.sleep(0.1)  # Small delay to ensure separate TCP packets
         client.sendall(otp.encode())  # Send the actual OTP
         response = client.recv(1024).decode()  # Receive the response
         return response
     
-def database_connection():
-    #TODO: Make sure to change the host, user, password, and database parameters 
-    database = mysql.connector.connect(
-        host="localhost",
-        user="yourusername",
-        password="yourpassword",
-        database="mydatabase"
-    )
-    ########
-
-    mycursor = database.cursor()
-
-    #TODO: Run your mysql query here to get the user's resource
-    mycursor.execute("SELECT * FROM user") #example
-    queryResult = mycursor.fetchall()
-    return queryResult
 
 def main():
-    otp_input = input("Please enter the OTP you received: ").strip()
-    response = verify_otp_with_server(otp_input)
-    print(f"Server response: {response}")
+    name = input("Username: ")
+    pssw = input("Password: ")
+    if verify_cred(name, pssw):
+        otp_input = input("Please enter the OTP you received: ").strip()
+        response = verify_otp_with_server(otp_input)
+        print(f"Server response: {response}")
 
-    if response == "Token verified successfully!":
-        db_resource = database_connection()
+        if response == "Token verified successfully!":
+            print(f"Access Granted\n")
 
-    print(f"Here is your resource:\n{db_resource}")
 
+    
 
 if __name__ == "__main__":
     main()
